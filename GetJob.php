88a68418327c22f1f742c2d6537dd5317474bc76
taylor-c-rauch <taylor.c.rauch@gmail.com>
2019-11-02 13:14:46 -0400

    some cosmetic changes, display avg ratings and display reviews on job page

--- GetJob.php
+++ GetJob.php
@@ -44,15 +44,16 @@
         </p>
       </div>
     </div>
-    <a class="btn btn-primary btn-sm" href="searchJobs.html" role="button">
-      Search Jobs
-    </a>
     <?php
         require "dbutil.php";
-        $db = DbUtil::loginConnection();
+        $db = DbUtil::logInUserB();
 
         $stmt = $db->stmt_init();
         $job_id = $_GET['jid'];
+        $backButton=$_GET['backButton'];
+        $review_count=0;
+
+        echo "<a class='btn btn-primary btn-sm' href=$backButton role='button'> Back </a>";
 
         if($stmt->prepare("select * from proj_job where job_id=$job_id") or die(mysqli_error($db))) {
                 $searchString = '%';
@@ -61,15 +62,49 @@
                 $stmt->bind_result($job_id, $title, $description, $hrs, $wages, $location, $work_study, $name);
                 
                 while($stmt->fetch()) {
-                        echo "<div class='card w-90'><div class='card-header'>$title</div><div class='card-body'><h5 class='card-title'>$name</h5><p class='card-text'>$description</br>Hourly Pay: $wages</p></div></div></br></br>" ;
+                        echo "<div class='card w-90'><div class='card-header'>$title</div><div class='card-body'><h5 class='card-title'>$name</h5><p class='card-text'>$description</br>Hourly Pay: $wages</p></div></div></br>" ;
                 }
                 $stmt->close();
         }
 
+        echo "<br/> <h5>Average Ratings</h5>";
+        $stmt = $db->stmt_init();
+
+        if($stmt->prepare("select avg(diff_rate), avg(boss_rate), avg(satisf_rate), avg(flexib_rate), count(rid) from proj_review where job_id=$job_id") or die(mysqli_error($db))) {
+          $searchString = '%';
+          $stmt->bind_param(s, $searchString);
+          $stmt->execute();
+          $stmt->bind_result($avg_diff, $avg_boss, $avg_satisf, $avg_flexib, $count);
+          
+          while($stmt->fetch()) {
+                  if($count==0){
+                    echo "<b>Difficulty: </b> ---<br/><b>Boss: </b>---<br/><b>Satisfaction: </b>---<br/><b>Flexibility: </b>---<br/>" ;
+                    break;
+                  }
+                  echo "<b>Difficulty: </b> $avg_diff<br/><b>Boss: </b>$avg_boss<br/><b>Satisfaction: </b>$avg_satisf<br/><b>Flexibility: </b>$avg_flexib<br/>" ;
+          }
+          $review_count=$count;
+          $stmt->close();
+  }
+        echo "<br/> <h5>Reviews</h5>";
+        $stmt = $db->stmt_init();
+
+        if($stmt->prepare("select * from proj_review where job_id=$job_id") or die(mysqli_error($db))) {
+          $searchString = '%';
+          $stmt->bind_param(s, $searchString);
+          $stmt->execute();
+          $stmt->bind_result($rid, $post_date, $post_time, $diff_rate, $boss_rate, $satisf_rate, $flexib_rate, $message, $cid, $job_id);
+          
+          if ($review_count==0){
+            echo "No reviews have been left on this job!";
+          } else{
+          while($stmt->fetch()) {
+                  echo "<div class='card w-90'><div class='card-header'>Difficulty: $diff_rate/5, Boss Rating: $boss_rate/5, Satisfaction: $satisf_rate/5, Flexibility: $flexib_rate/5</div><div class='card-body'><p class='card-text'>$message</p><div class='card-footer text-muted'>$cid, $post_date</div></div></div></br></br>" ;
+          }
+        }
+          $stmt->close();
+  }
         $db->close();
 ?>
-    <center>
-      <div id="jobresult"></div>
-    </center>
   </body>
 </html>
--- library.php
+++
@@ -1,6 +0,0 @@
-<?php
-$SERVER = 'cs4750.cs.virginia.edu';
-$USERNAME = 'eyc4xd';
-$PASSWORD = '';
-$DATABASE = 'eyc4xd';
-?>
