dca45417d024ebd7924f26cf20e1dc7aacf06f55
Jay Park <jaypark@d-172-25-255-241.dhcp.virginia.edu>
2019-11-04 20:42:57 -0500

    db

--- dbutil.php
+++ dbutil.php
@@ -1,9 +1,9 @@
 <?php
 class DbUtil{
-	public static $loginUser = "tcr4nd"; 
+	public static $loginUser = "jp6ud";
 	public static $loginPass = "";
 	public static $host = "cs4750.cs.virginia.edu"; // DB Host
-	public static $schema = "tcr4nd_project"; // DB Schema
+	public static $schema = "jp6ud_nadabases"; // DB Schema
 
 	public static function loginConnection(){
 			$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
