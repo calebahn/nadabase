43339ef1869b3b7d248f034c6e838cde0defba6c
taylor-c-rauch <taylor.c.rauch@gmail.com>
2019-11-13 00:56:12 -0500

    get favorite, curr and prev work buttons going

--- GetJob.php
+++ GetJob.php
@@ -23,6 +23,102 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="styles.css">
     <title>Job Description</title>
+    <style>
+      /* Popup Box */
+      /* The Modal (background) */
+      .modal {
+      display: none; /* Hidden by default */
+      position: fixed; /* Stay in place */
+      z-index: 8888; /* Sit on top */
+      left: 0;
+      top: 0;
+      width: 100%; /* Full width */
+      height: 100%; /* Full height */
+      overflow: auto; /* Enable scroll if needed */
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+      }
+      /* Modal Content/Box */
+      .modal-content {
+      background-color: #fefefe;
+      margin: 10vh auto; /* 15% from the top and centered */
+      padding: 20px;
+      border: 1px solid #888;
+      width: 60%; /* Could be more or less, depending on screen size */
+      }
+      @media (min-width: 1366px) {
+      .modal-content {
+      background-color: #fefefe;
+      margin: 10vh auto; /* 15% from the top and centered */
+      padding: 20px;
+      border: 1px solid #888;
+      width: 30%; /* Could be more or less, depending on screen size */
+      }
+      }
+      h2, p {
+      margin: 0 0 20px;
+      font-weight: 400;
+      color: #666;
+      }
+      span{
+      color: #666;
+      display:block;
+      padding:0 0 5px;
+      }
+      form {
+      padding: 25px;
+      margin: 25px;
+      box-shadow: 0 2px 5px #f5f5f5; 
+      background: #eee; 
+      }
+      input, textarea {
+      width: calc(100% - 18px);
+      padding: 8px;
+      margin-bottom: 20px;
+      border: 1px solid #1c87c9;
+      outline: none;
+      }
+      .contact-form button {
+      width: 100%;
+      padding: 10px;
+      border: none;
+      background: #1c87c9; 
+      font-size: 16px;
+      font-weight: 400;
+      color: #fff;
+      }
+      button:hover {
+      background: #2371a0;
+      }    
+      /* The Close Button */
+      .close {
+      color: #aaa;
+      float: right;
+      font-size: 28px;
+      font-weight: bold;
+      }
+      .close:hover,
+      .close:focus {
+      color: black;
+      text-decoration: none;
+      cursor: pointer;
+      }
+      button.button {
+      background:none;
+      border-top:none;
+      outline: none;
+      border-right:none;
+      border-left:none;
+      border-bottom:#02274a 1px solid;
+      padding:0 0 3px 0;
+      font-size:16px;
+      cursor:pointer;
+      }
+      button.button:hover {
+      border-bottom:#a99567 1px solid;
+      color:#a99567;
+      }
+    </style>
   </head>
   <body>
     <div id='navbar'>
@@ -52,6 +148,71 @@
         </p>
       </div>
     </div>
+    <p>
+      <button class="button" data-modal="modalOne">Previously Work</button>
+    </p>
+    <p>
+      <button class="button" data-modal="modalTwo">Currently Work</button>
+    </p>
+    <p>
+      <a class='button'  href='buttons.php' role='button'>Favorite</a>
+    </p>
+
+    <div id="modalOne" class="modal">
+      <div class="modal-content">
+        <div class="contact-form">
+          <a class="close">&times;</a>
+          <form action="buttons.php" method="post">
+            <h2>Work Information</h2>
+            <div> 
+              <input required type="text" name="pstart" placeholder="Start Date">
+              <input required type="text" name="pend" placeholder="End Date">
+            </div>
+            <button type="submit" href="buttons.php">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <div id="modalTwo" class="modal">
+      <div class="modal-content">
+        <div class="contact-form">
+          <span class="close">&times;</span>
+          <form action="buttons.php" method="post">
+            <h2>Work Information</h2>
+            <div>
+              <input required type="num" name="cstart" placeholder="Start Date(yyyy-mm-dd)">
+            </div>
+            <button type="submit" href="buttons.php">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <script>
+      var modalBtns = [...document.querySelectorAll(".button")];
+      modalBtns.forEach(function(btn){
+        btn.onclick = function() {
+          var modal = btn.getAttribute('data-modal');
+          document.getElementById(modal).style.display = "block";
+        }
+      });
+      
+      var closeBtns = [...document.querySelectorAll(".close")];
+      closeBtns.forEach(function(btn){
+        btn.onclick = function() {
+          var modal = btn.closest('.modal');
+          modal.style.display = "none";
+        }
+      });
+      
+      window.onclick = function(event) {
+        if (event.target.className === "modal") {
+          event.target.style.display = "none";
+        }
+      }
+    </script>
     <?php
         require "dbutil.php";
         session_start();
@@ -161,22 +322,22 @@
         $word_count=count($words);
         echo "<form action='ReviewInsert.php?job_id=$job_id' method='post'>
         <div class='input-group'>
-          Review: <textarea class='form-control' rows='5' type='text' name='review'></textarea>
+          Review: <textarea class='form-control' rows='5' type='text' name='review' required></textarea>
         </div><br/>
         <div class='input-group'>
-          Difficulty Rating:<input class='form-control' type='number' name='diff' />
+          Difficulty Rating:<input class='form-control' type='number' name='diff' required/>
           <br/><br/>
         </div>
         <div class='input-group'>
-          Boss Rating:<input class='form-control' type='number' name='boss' />
+          Boss Rating:<input class='form-control' type='number' name='boss' required/>
           <br/><br/>
         </div>
         <div class='input-group'>
-          Satisfaction Rating:<input class='form-control' type='number' name='satisf' />
+          Satisfaction Rating:<input class='form-control' type='number' name='satisf' required />
           <br/><br/>
         </div>
         <div class='input-group'>
-          Flexibility Rating:<input class='form-control' type='number' name='flex' />
+          Flexibility Rating:<input class='form-control' type='number' name='flex' required/>
           <br/><br/>
         </div>
         <div class='dropdown'>
