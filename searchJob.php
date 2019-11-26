83b0e623cc635f0a9b36a73293a875a9ec8370a6
CalebAhn <cha4yw@virginia.edu>
2019-11-26 18:05:18 -0500

    fix style of search bar and style of results

--- searchJob.php
+++ searchJob.php
@@ -13,7 +13,51 @@
                 $stmt->bind_result($job_id, $title, $description, $hrs, $wages, $location, $work_study, $name);
                 
                 while($stmt->fetch()) {
-                        echo "<div class='card w-90'><div class='card-header'>$title</div><div class='card-body'><h5 class='card-title'>$name</h5><p class='card-text'>$description</br>Hourly Pay: $wages</p></div><a class='btn btn-primary btn-sm' href='GetJob.php?jid=$job_id' role='button'>More Information</a></div></br></br>" ;
+                        /*
+                        echo "
+                        <div class='card w-90'>
+                        <div class='card-header'>
+                        $title
+                        </div>
+                        <div class='card-body'>
+                        <h5 class='card-title'>
+                        $name
+                        </h5>
+                        <p class='card-text'>
+                        $description</br>
+                        Hourly Pay: $wages
+                        </p>
+                        </div>
+                        <a class='btn btn-primary btn-sm' href='GetJob.php?jid=$job_id' role='button'>
+                        More Information
+                        </a>
+                        </div>
+                        </br></br>" ;
+                        */
+                        echo "
+                        <div class='main'>
+                                <div class='card w-90'>
+                                        <div class='card-header'>
+                                                $name $title
+                                        </div>
+                                <div class='card-body'>
+                                <h5 class='card-title'>
+                                        $name
+                                </h5>
+                                <p class='card-text'>
+                                        $description</br>
+                                        Hourly Pay: $$wages
+                                </p>
+                                </div>
+                                        <a class='btn btn-primary btn-sm' href='GetJob.php?jid=$job_id' role='button'>
+                                                More Information
+                                        </a>
+                                </div>
+                        </div>
+
+                        </br>
+                        </br>
+                        " ;
                 }
                 $stmt->close();
         }
